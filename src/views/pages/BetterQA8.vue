a7ed6c1f49d98ba38d70f1279f750d9edd6b9acc
LayMui <laymui.toh@taiger.com>
2021-03-07 09:08:38 +0800

    Add credit

--- src/views/pages/BetterQA8.vue
+++ src/views/pages/BetterQA8.vue
@@ -2,6 +2,9 @@
   <div class="text-center">
     <img  contain height=auto width=100%  src="../../assets/image/BetterQA8.png"/>
  
-   
+    <p class="text-muted text-center">
+      Credit: <a href="https://johnfergusonsmart.com/" target="_blank">John Smart</a>
+    </p>
 </div>
 </template>
+
